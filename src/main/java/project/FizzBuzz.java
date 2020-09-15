5aeb46c36e9e5fe5e595b5896c4dc7d3d5a5b688
Alyssha Timm <aly.nelso@gmail.com>
2020-09-15 13:50:15 -0400

    FizzBuzz interpret method finished

--- src/main/java/project/FizzBuzz.java
+++ src/main/java/project/FizzBuzz.java
@@ -4,6 +4,17 @@ public class FizzBuzz {
 
 
     public String interpret(int numberToSay) {
-        return "1";
+        if(numberToSay % 3 == 0 && numberToSay % 5 ==0){
+            return "FizzBuzz";
+        }
+        if(numberToSay % 3 == 0){
+            return "Fizz";
+            //if number is 3, it will exit the method. if not, code proceeds to the below
+        }
+        if(numberToSay % 5 == 0){
+            return "Buzz";
+        }
+        return "" + numberToSay;
+        //concatenates empty string to int value to produce number
     }
 }
